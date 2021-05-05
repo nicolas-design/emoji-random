f14ac3d39213648dcf85d523e5ab8401b9ecab67
Nicolas <gadnernicolas@gmail.com>
2021-05-05 22:57:45 +0200

    Updatet fully funct

--- index.js
+++ index.js
@@ -14,39 +14,82 @@ function createArray(arrEx, length, width) {
   }
 }
 //
+function arrReplace(arr, length, width) {
+  let middleL = Math.floor(length / 2);
+  let middleW = Math.floor(width / 2);
+  for (let i = middleW - 1; i <= middleW + 1; i++) {
+    for (let x = middleL - 10; x <= middleL + 10; x++) {
+      arr[i][x] = ' ';
+    }
+  }
+}
+
+function hexArr(arr, length, width, str) {
+  let middleL = Math.floor(length / 2);
+  let middleW = Math.floor(width / 2);
+  let text = str;
+  //text = str;
+  let charC = 0;
+  for (let i = middleW; i < middleW + 1; i++) {
+    for (let x = middleL - 3; x < middleL + 3; x++) {
+      arr[i][x] = text.charAt(charC);
+      charC++;
+    }
+  }
+}
 //
+function printArr(arr, length, width, hex) {
+  let useColor = hex;
+  hexArr(arr, length, width, useColor);
+  for (let i = 0; i < width; i++) {
+    console.log();
+    for (let x = 0; x < length; x++) {
+      process.stdout.write(chalk.hex(useColor)(arr[i][x]));
+    }
+  }
+  console.log();
+}
 //
 //
 
-function print(ex, length, Width) {
+function print() {
   let bool = true;
   while (bool) {
-    let input1 = prompt('Enter luminosity: ');
-    let input2 = prompt('Enter hue: ');
-
-    if (input1 === 'ask' || input2 === 'ask') {
-      console.log('Enter a color!');
-    } else if (input2 === 'random') {
-      let useColor = randomColor();
-      for (let i = 0; i < Width; i++) {
-        console.log();
-        for (let x = 0; x < length; x++) {
-          process.stdout.write(chalk.hex(useColor)(ex[i][x]));
-        }
-      }
-    } else if (input1 === 'quit' || input2 === 'quit') {
+    let input1 = prompt('input: ');
+    let inp = input1.split(' ');
+    if (input1 === '') {
+      let arr = [];
+      createArray(arr, 31, 9);
+      arrReplace(arr, 31, 9);
+      printArr(arr, 31, 9, randomColor());
+    } else if (inp.length === 1 && inp[0] === 'ask') {
+      console.log('Enter color and luminosity!');
+    } else if (inp.length === 1 && inp[0] === 'random') {
+      let arr = [];
+      createArray(arr, 31, 9);
+      arrReplace(arr, 31, 9);
+      printArr(arr, 31, 9, randomColor());
+    } else if (inp.length === 1 && inp[0] === 'quit') {
       bool = false;
-    } else if (input1 !== undefined && input2 !== undefined) {
+    } else if (inp.length === 2) {
       let hexcode = randomColor({
-        luminosity: input1,
-        hue: input2,
+        luminosity: inp[0],
+        hue: inp[1],
       });
-      for (let i = 0; i < Width; i++) {
-        console.log();
-        for (let x = 0; x < length; x++) {
-          process.stdout.write(chalk.hex(hexcode)(ex[i][x]));
-        }
-      }
+      let arr = [];
+      createArray(arr, 31, 9);
+      arrReplace(arr, 31, 9);
+      printArr(arr, 31, 9, hexcode);
+    } else if (inp.length === 3 && inp[0].includes('x')) {
+      let leandwi = inp[0].split('x');
+      let hexcode = randomColor({
+        luminosity: inp[1],
+        hue: inp[2],
+      });
+      let arr = [];
+      createArray(arr, leandwi[0], leandwi[1]);
+      arrReplace(arr, leandwi[0], leandwi[1]);
+      printArr(arr, leandwi[0], leandwi[1], hexcode);
     } else {
       console.log('error');
     }
@@ -60,13 +103,8 @@ function print(ex, length, Width) {
 
 //main
 let input;
-console.log('Type start to start the game');
-do {
-  input = prompt('input: ').toLowerCase();
-} while (input !== 'start');
-console.log('Type quit to quit the game');
 let arr = [];
-let squareA = prompt('Enter length: ');
-let squareB = prompt('Enter Width: ');
-createArray(arr, squareA, squareB);
-print(arr, squareA, squareB);
+createArray(arr, 31, 9);
+arrReplace(arr, 31, 9);
+printArr(arr, 31, 9, randomColor());
+print();
